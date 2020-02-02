08b0be178cedffabb9e4be18cdd9d3313f1d45ff
lhmagic <lhmagic@139.com>
2020-02-02 22:17:01 +0800

    do not swap the calculated value.

--- CrcCalc.c
+++ CrcCalc.c
@@ -24,46 +24,49 @@ static uint32_t crcTable[256] = {0};
 #endif
 
 /******************* Construct CRC fast lookup table *********************
- * @brief Generic cyclic redundancy check fast lookup table.
- *        To avoid bit reverse for every byte, we just reverse the polymorphic.
- *		  The calculated result stored in the crcTable array by MSB first.
+ * @brief Generate a cyclic redundancy check fast lookup table.
+ *        To avoid bit reverse for every byte,
+ *        we reverse the polymorphic if needed.
  * @param none.
  * @retval none.
  ************************************************************************/
 void CreateCrcTable(void) {
 #if defined	(CRC_8) || defined (CRC_8_ITU) || defined (CRC_8_ROHC) || defined(CRC_8_MAXIM)
+uint8_t crc = 0;
 #if (CRC_BIT_REV == true)
 uint8_t poly = ReverseUint8(CRC_POLY);
+uint8_t mask = 0x01;
 #else
 uint8_t poly = CRC_POLY;
-uint8_t msb = 0x80;
+uint8_t mask = 0x80;
 #endif
-uint8_t crc = 0;
 #elif defined (CRC_16_IBM) || defined (CRC_16_MAXIM) || defined (CRC_16_USB) || \
       defined (CRC_16_MODBUS) || defined (CRC_16_CCITT) ||defined (CRC_16_CCITT_FALSE) || \
       defined (CRC_16_X25) || defined (CRC_16_XMODEM) || defined (CRC_16_DNP)
+uint16_t crc = 0;
 #if (CRC_BIT_REV == true)
 uint16_t poly = ReverseUint16(CRC_POLY);
+uint16_t mask = 0x0001;
 #else
 uint16_t poly = CRC_POLY;
-uint16_t msb = 0x8000;
+uint16_t mask = 0x8000;
 #endif
-uint16_t crc = 0;
 #elif defined (CRC_32) || defined (CRC_32_MPEG_2)
+uint32_t crc = 0;
 #if (CRC_BIT_REV == true)
 uint32_t poly = ReverseUint32(CRC_POLY);
+uint32_t mask = 0x00000001;
 #else
 uint32_t poly = CRC_POLY;
-uint32_t msb = 0x80000000;
+uint32_t mask = 0x80000000;
 #endif
-uint32_t crc = 0;
 #endif
 
     for(uint16_t i=0; i<256; i++) {
         crc = 0;
 #if (CRC_BIT_REV == true)
         for(uint8_t j=0x01; j!=0; j<<=1) {
-            crc = (crc & 0x01) ? ((crc >> 1) ^ poly) : (crc >> 1);
+            crc = (crc & mask) ? ((crc >> 1) ^ poly) : (crc >> 1);
             crc = (i & j) ? (crc ^ poly) : crc;
         }
 #if defined (CRC_8) || defined (CRC_8_ITU) || defined (CRC_8_ROHC) || defined(CRC_8_MAXIM)
@@ -77,26 +80,18 @@ uint32_t crc = 0;
 #endif
 #else
         for(uint8_t j=0x80; j!=0; j>>=1) {
-            crc = (crc & msb) ? ((crc << 1) ^ poly) : (crc << 1);
+            crc = (crc & mask) ? ((crc << 1) ^ poly) : (crc << 1);
             crc = (i & j) ? (crc ^ poly) : crc;
         }
-#if defined (CRC_8) || defined (CRC_8_ITU) || defined (CRC_8_ROHC) || defined(CRC_8_MAXIM)
-        crcTable[i] = crc;
-#elif defined (CRC_16_IBM) || defined (CRC_16_MAXIM) || defined (CRC_16_USB) || \
-      defined (CRC_16_MODBUS) || defined (CRC_16_CCITT) ||defined (CRC_16_CCITT_FALSE) || \
-      defined (CRC_16_X25) || defined (CRC_16_XMODEM) || defined (CRC_16_DNP)             
-        crcTable[i] = crc;
-#elif defined (CRC_32) || defined (CRC_32_MPEG_2)
-        crcTable[i] = crc;
-#endif        
+        crcTable[i] = crc;      
 #endif
     }
 }
 
 #if defined	(CRC_8) || defined (CRC_8_ITU) || defined (CRC_8_ROHC) || defined(CRC_8_MAXIM)
 /******************* Cyclic Redundancy Check Calculate *********************
- * @brief Generic crc-8 calculate.
- *        To avoid bit reverse for every byte, we reverse the polymorphic.
+ * @brief Generic crc-8 calculate by table-driven.
+ *
  * @param buff : character buff start address.
  *		  cnt : character buff count.
  * @retval CRC calculate result.
@@ -115,8 +110,8 @@ uint8_t crc = CRC_INIT;
       defined (CRC_16_MODBUS) || defined (CRC_16_CCITT) ||defined (CRC_16_CCITT_FALSE) || \
       defined (CRC_16_X25) || defined (CRC_16_XMODEM) || defined (CRC_16_DNP)
 /******************* Cyclic Redundancy Check Calculate *********************
- * @brief Generic crc-16 calculate.
- *		  The calculated result already be swaped in the fast lookup table.
+ * @brief Generic crc-16 calculate  by table-driven.
+ *
  * @param buff : character buff start address.
  *		  cnt : character buff count.
  * @retval CRC calculate result, MSB first.
@@ -129,16 +124,16 @@ uint16_t crc = CRC_INIT;
 	}
 
 #if (CRC_BIT_REV == true)
-  return (crc ^ CRC_OUT_XOR);
-#else
   return SwapUint16(crc ^ CRC_OUT_XOR);
+#else
+  return (crc ^ CRC_OUT_XOR);
 #endif
 }
 
 #elif defined (CRC_32) || defined (CRC_32_MPEG_2)
 /******************* Cyclic Redundancy Check Calculate *********************
- * @brief Generic crc-32 calculate.
- *		  The calculated result already be swaped in the fast lookup table.
+ * @brief Generic crc-32 calculate  by table-driven.
+ *
  * @param buff : character buff start address.
  *		  cnt : character buff count.
  * @retval CRC calculate result, MSB first.
@@ -151,9 +146,9 @@ uint32_t crc = CRC_INIT;
 	}
 
 #if (CRC_BIT_REV == true)
-  return (crc ^ CRC_OUT_XOR);
-#else
   return SwapUint32(crc ^ CRC_OUT_XOR);
+#else
+  return (crc ^ CRC_OUT_XOR);
 #endif
 }
 #endif
